bc3f03759fc9c889640e41915b4a605655c15806
omar5285 <oayesh70@gmail.com>
2025-01-31 15:44:29 +0300

    88

--- src/App.tsx
+++ src/App.tsx
@@ -172,7 +172,7 @@ function App() {
 
         {/* Header */}
         <header className="container mx-auto px-4 py-8">
-          
+         
           <h1 className="text-5xl font-bold mb-4 text-white">
             {t.cantonal_logo}
           </h1>
